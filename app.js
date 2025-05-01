165794e7e2a01b22b1a55d5001c3f7111b870d5a
Kajal Koli <kajalkoli20@gmail.com>
2025-05-01 21:27:13 +0530

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Node.js server! Hello there.. we aare  trying to run the github workflows locally');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
